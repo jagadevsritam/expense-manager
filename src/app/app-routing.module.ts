c410cf838826d652d1f20e4ab560f3fed39ea4ee
sritamjagadev <jagadevsritam@gmail.com>
2023-06-05 23:46:00 +0530

    routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
-
+import { Routes, RouterModule } from '@angular/router'; import { ExpenseEntryComponent } from './expense-entry/expense-entry.component';
+import { ExpenseEntryListComponent } from './expense-entry-list/expense-entry-list.component';
+const routes: Routes = [
+   { path: 'expenses', component: ExpenseEntryListComponent },
+   { path: 'expenses/detail/:id', component: ExpenseEntryComponent },
+   { path: '', redirectTo: 'expenses', pathMatch: 'full' }];
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
-})
+   imports: [RouterModule.forRoot(routes)],
+   exports: [RouterModule] })
 export class AppRoutingModule { }
